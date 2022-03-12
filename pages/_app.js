098d3a54d0bfb5013aaaf5404d64ee9da8fb42e6
Alexander Spiridonov <asp@stillhuman.com>
2022-03-12 17:17:39 +0300

    clean up nextjs scaffolding

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
-  return <div>Testing...</div>;
-}
-
-export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DefaultPage: React.FC = () => {
+  return <div>default</div>;
+};
+
+export default DefaultPage;
