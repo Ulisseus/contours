c711b2dca35e1cbf30c37c909e736554a4cffc51
Alexander Spiridonov <asp@stillhuman.com>
2022-03-19 10:26:33 +0300

    add title to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const Header: React.FC = () => {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoBox}>
+        <img src="/logo-white.png" alt="logo" className={styles.logo} />
+      </div>
+      <div className={styles.titleBox}>
+        <h1 className={styles.title}>
+          <span className={styles.titleMain}>Outdoors</span>
+          <span className={styles.titleSub}>is where the life happens</span>
+        </h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
