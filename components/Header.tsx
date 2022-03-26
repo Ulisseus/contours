cf07539d1591a8717bf7212ec30d77e9c1e850f6
Alexander Spiridonov <asp@stillhuman.com>
2022-03-26 10:37:44 +0300

    update sass files structure

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,9 @@ const Header: React.FC = () => {
         <img src="/logo-white.png" alt="logo" className={styles.logo} />
       </div>
       <div className={styles.titleBox}>
-        <h1 className={styles.title}>
-          <span className={styles.titleMain}>Outdoors</span>
-          <span className={styles.titleSub}>is where the life happens</span>
+        <h1 className={styles.heading}>
+          <span className={styles.headingMain}>Outdoors</span>
+          <span className={styles.headingSub}>is where the life happens</span>
         </h1>
         <a className={styles.button} href="#">
           Discover our tours
