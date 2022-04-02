6034b9f1bda6f5499c955578ab6253782b4d5430
Alexander Spiridonov <asp@stillhuman.com>
2022-04-02 14:59:20 +0300

    add Card component

--- components/Tours/index.tsx
+++ components/Tours/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { Card } from "../Card";
 
 export const Tours: React.FC = () => {
   return (
@@ -7,10 +8,16 @@ export const Tours: React.FC = () => {
       <div className="center-text mb-big">
         <h2 className="h-2 h-2_animated h-2_background">Most popular course</h2>
       </div>
-      <div className="tours d-flex">
-        <div className="col-4">First</div>
-        <div className="col-4">Second</div>
-        <div className="col-4">Third</div>
+      <div className={`${styles.columnWrapper} d-flex`}>
+        <div className="col-4">
+          <Card title="The sea explorer" />
+        </div>
+        <div className="col-4">
+          <Card title="Second" />
+        </div>
+        <div className="col-4">
+          <Card title="Third" />
+        </div>
       </div>
     </section>
   );
