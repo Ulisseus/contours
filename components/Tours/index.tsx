011e182418d5f1f42b92e1fb0d1a1c74f4d658c3
Alexander Spiridonov <asp@stillhuman.com>
2022-04-02 14:41:23 +0300

    add tours layout

--- components/Tours/index.tsx
+++ components/Tours/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 export const Tours: React.FC = () => {
   return (
-    <section className="tours">
-      <h2 className="h-2 h-2_animated h-2_background">Most popular course</h2>
+    <section className={styles.wrapper}>
+      <div className="center-text mb-big">
+        <h2 className="h-2 h-2_animated h-2_background">Most popular course</h2>
+      </div>
       <div className="tours d-flex">
         <div className="col-4">First</div>
         <div className="col-4">Second</div>
